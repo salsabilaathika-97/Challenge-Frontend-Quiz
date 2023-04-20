d31d96def1f03bed23291a95a47c8f940a7a0e15
salsabilaathika-97 <salsathikar@gmail.com>
2023-04-20 14:56:07 +0800

    create login form

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,9 +1,38 @@
 import react from "react";
+import {Col, Button, Row, Container, Card, Form} from "react-bootstrap";
 
 const Login = () => {
     return (
         <div>
-            <h1>Halaman Login</h1>
+            <Container>
+                <Row className="vh-100 d-flex justify-content-center align-items-center">
+                    <Col md = {8} lg={6} xs={12}>
+                        <Card className="shadow">
+                            <Card.Body>
+                                <div className="mb-3 mt-md-4">
+                                    <h2 className="fw-bold mb-2 text-uppercase">Welcome</h2>
+                                    <p className="mb-5">Please enter your username</p>
+                                    <div className="mb-3">
+                                        <Form>
+                                            <Form.Group className="mb-3" controlId="formBasicEmail">
+                                                <Form.Label className="text-center">
+                                                    Username
+                                                </Form.Label>
+                                                <Form.Control placeholder="Enter username" required />
+                                            </Form.Group>
+                                            <div className="d-grid">
+                                                <Button variant="primary" type="submit">
+                                                Login
+                                                </Button>
+                                            </div>
+                                        </Form>
+                                    </div>
+                                </div>
+                            </Card.Body>
+                        </Card>
+                    </Col>
+                </Row>
+            </Container>
         </div>
     )
 }
